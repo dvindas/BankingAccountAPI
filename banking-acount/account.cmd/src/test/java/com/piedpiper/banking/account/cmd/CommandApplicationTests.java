d5e8ebac8f824127cb9a8f7485acc27e76aedbca
dvindas <41488048+dvindas@users.noreply.github.com>
2023-04-07 16:35:51 -0600

    Added app based project structure and account based projects.

---
+++ banking-acount/account.cmd/src/test/java/com/piedpiper/banking/account/cmd/CommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piedpiper.banking.account.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
